94d59ad76ba8e598a370c967652cba1fd6db183a
mbhaskar98 <bhaskarmahajan98@gmail.com>
2021-04-26 00:17:11 +0530

    Moon Follow Player

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,15 +6,21 @@ public class CameraFollow : MonoBehaviour
 {
     private Transform player;
 
+    private Transform moon;
     private Vector3 tempPos;
 
     private string PLAYER_TAG = "Player";
+
+    private string MOON_TAG = "Moon_Tag";
     // Start is called before the first frame update
 
     private float minX, maxX;
     void Start()
     {
-        player = GameObject.FindWithTag(PLAYER_TAG).transform;   
+        player = GameObject.FindWithTag(PLAYER_TAG).transform; 
+        moon = GameObject.FindWithTag(MOON_TAG).transform;
+        moon.position  = new Vector3(0f, moon.position.y, moon.position.z);  
+        Debug.Log(moon.position);
         minX = (float)-61;
         maxX = (float)61;
     }
@@ -33,6 +39,9 @@ public class CameraFollow : MonoBehaviour
         }
         
         transform.position = tempPos;
+        tempPos.x -= (float)7;
+        
+        moon.position = new Vector3(tempPos.x, moon.position.y, moon.position.z);
         
     }
 }
